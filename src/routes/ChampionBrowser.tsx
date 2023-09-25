287538c9e5521112647c209a013927ce85fd6a2c
matssg <40918636+NODDZY@users.noreply.github.com>
2023-09-25 15:30:14 +0200

    Basic searchbar

--- src/routes/ChampionBrowser.tsx
+++ src/routes/ChampionBrowser.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import { fetchChampionData, fetchCurrentVersion } from "../services/league/api";
 import { capitalizeFirstLetter } from "../services/league/utils";
+import ChampionSearchBar from "../components/ChampionSearchBar";
 import "../styles/champion-browser.css";
 
 export default function ChampionBrowser() {
   const [version, setVersion] = useState<string | null>(null);
-  const [champions, setChampions] = useState<Object | null>(null);
+  const [champions, setChampions] = useState<Champion[] | null>(null);
+
+  const [filteredChampions, setFilteredChampions] = useState<Champion[] | null>(null);
 
   useEffect(() => {
     async function fetchList() {
@@ -14,20 +17,30 @@ export default function ChampionBrowser() {
       if (currentVersion) {
         const currentChampions = await fetchChampionData(currentVersion);
         setChampions(currentChampions);
+        setFilteredChampions(currentChampions);
       }
     }
 
     fetchList();
   }, []);
 
+  const handleSearch = (searchTerm: string) => {
+    if (champions) {
+      const filtered = Object.values(champions).filter((champion) => champion.name.toLowerCase().includes(searchTerm.toLowerCase()));
+      setFilteredChampions(filtered);
+    }
+  };
+
   return (
     <div className="main-element">
       <h1>League Champion Browser</h1>
       <p>Browse all League of Legends champions for the current patch.</p>
 
+      <ChampionSearchBar onSearch={handleSearch} />
+
       <div className="champion-grid">
-        {champions &&
-          Object.values(champions).map((champion) => (
+        {filteredChampions &&
+          Object.values(filteredChampions).map((champion) => (
             <a
               key={champion.key}
               className="champion-item"
@@ -36,7 +49,7 @@ export default function ChampionBrowser() {
               <div className="champion-image">
                 <img
                   src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.id}.png`}
-                  alt={champion.name}
+                  alt={`${champion.id.toLowerCase()}.png`}
                 />
               </div>
               <div className="champion-info">
