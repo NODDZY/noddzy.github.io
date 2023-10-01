4c4e17c5e29821ee8f340f081d179b05c1eb84c3
matssg <40918636+NODDZY@users.noreply.github.com>
2023-10-01 15:39:05 +0200

    Minor style changes

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -9,15 +9,15 @@ export default function Index() {
       <p className="index-text">Single page application made with React + Vite and TypeScript.</p>
       <p className="index-text">Serverless static website deployed to GitHub Pages.</p>
 
-      <div id="jaina-quote">
-        <p className="jaina-text">Arguably, it (that is, some object) exists.</p>
-        <p className="jaina-text">Arguably, it does not exist.</p>
-        <p className="jaina-text">Arguably, it exists; arguably, it doesn't exist.</p>
-        <p className="jaina-text">Arguably, it is non-assertible.</p>
-        <p className="jaina-text">Arguably, it exists; arguably, it is non-assertible.</p>
-        <p className="jaina-text">Arguably, it doesn't exist; arguably, it is non-assertible.</p>
-        <p className="jaina-text">Arguably, it exists; arguably, it doesn't exist; arguably it is non-assertible.</p>
-      </div>
+      {/*
+        Arguably, it (that is, some object) exists.
+        Arguably, it does not exist.
+        Arguably, it exists; arguably, it doesn't exist.
+        Arguably, it is non-assertible.
+        Arguably, it exists; arguably, it is non-assertible.
+        Arguably, it doesn't exist; arguably, it is non-assertible.
+        Arguably, it exists; arguably, it doesn't exist; arguably it is non-assertible.
+      */}
     </div>
   );
 }
